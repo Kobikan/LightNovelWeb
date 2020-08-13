7debb6d731a78aeb3fba52a79095db4089a8eccd
Kobikan <kobikan.linganathan@gmail.com>
2020-08-13 02:09:13 -0400

    Finish Login page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router, } from 'react-router-dom';
 import Routes from './services/routes';
 import history from './services/history';
 

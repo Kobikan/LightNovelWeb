14b36f989ba6f2d8bf6a1e3211fb5f19093337cf
Kobikan <kobikan.linganathan@gmail.com>
2020-08-14 20:52:24 -0400

    Nav Bar

--- src/login/loginController.js
+++ src/login/loginController.js
@@ -1,21 +1,21 @@
 const axios = require('axios');
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001',
-  timeout: 1000,
+	baseURL: 'http://localhost:3001',
+	timeout: 1000,
 });
 
 class LoginController {
-  async login(credentials) {
-    let res = null;
-    const { email, password} = credentials;
-      return await instance.post('/login/login', {
-        email,
-        password
-      }).catch((e) => {
-        res = e.response;
-      });
-  }
+	async login(credentials) {
+		let res = null;
+		const { email, password} = credentials;
+		return await instance.post('/login/login', {
+			email,
+			password
+		}).catch((e) => {
+			res = e.response;
+		});
+	}
 }
 
 export default LoginController;
